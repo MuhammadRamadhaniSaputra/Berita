4ef8ac112030dd1704163f7f88f6be6ec7afd755
Dani <2011102441087@umkt.ac.id>
2022-12-23 10:44:58 +0800

    register

--- mytest/settings.py
+++ mytest/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     #Install apps
     'data',
+    'users',
     'ckeditor',
     'ckeditor_uploader',
 
---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Biodata(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telp = models.CharField(max_length=14)
+    
+
+    def __str__(self):
+        return "{} - {}".format(self.user, self.user)
